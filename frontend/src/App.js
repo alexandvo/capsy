64d1fe741050e2d4d22751dfa8be8af960ee385d
Alex Vo <alexanvo@tamu.edu>
2024-02-03 14:57:10 -0600

    making empty page for home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
-import Login from "./pages/login";
-import Signup from "./pages/signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 function App() {
-  return <Signup/>;
+  return <Home/>;
 }
 
 export default App;
