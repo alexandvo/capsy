11d1cc401380635850ed640cc4982b1506a4b456
Alex Vo <alexanvo@tamu.edu>
2024-02-14 16:56:58 -0600

    added navigation menu dropdown

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,30 @@
 import "../stylesheets/header.css";
 import icon from "../assets/imgs/time-capsule-icon.png";
 import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
 const Header = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+  const profilePicRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        event.target !== profilePicRef.current
+      ) {
+        setShowDropdown(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
   return (
     <div className="rect">
       <div id="iconPicContainer">
@@ -12,9 +34,21 @@ const Header = () => {
       </div>
 
       <h1>Capsy</h1>
-      <div id="profilePicContainer">
-        <div id="pfpCir"></div>
+      <div
+        id="profilePicContainer"
+        onClick={() => {
+          setShowDropdown(!showDropdown);
+        }}
+      >
+        <div id="pfpCir" ref={profilePicRef}></div>
       </div>
+      {showDropdown && (
+        <div id="dropdown" ref={dropdownRef}>
+          <Link to="/capsules" className="options">Capsules</Link>
+          <Link to="/settings" className="options">Settings</Link>
+          <Link className="options">Logout</Link>
+        </div>
+      )}
     </div>
   );
 };
