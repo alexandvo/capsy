2c0d3c2f9285bf5eab9b9d1ee2c26de1c84e030d
Alex Vo <alexanvo@tamu.edu>
2024-03-31 00:52:11 -0500

    started filing uploading

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,18 +2,47 @@ import Layout from "../components/Layout";
 import "../stylesheets/home.css";
 import "../stylesheets/global.css";
 import plus from "../assets/imgs/plus.png";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import CapsuleList from "./CapsuleList";
-import CapsuleInfo from "./CapsuleInfo";
+// import CapsuleList from "./CapsuleList";
+// import CapsuleInfo from "./CapsuleInfo";
 
 const Home = () => {
+  const fileInputRef = useRef();
+
   const [showForm, setShowForm] = useState(false);
 
   const [showComponent, setShowComponent] = useState(true);
 
   const [startDate, setStartDate] = useState(new Date());
+
+  const [selectedFiles, setSelectedFiles] = useState([]);
+
+  function handleUploadContentClick() {
+    fileInputRef.current.click();
+  }
+
+  function handleFileChange(event) {
+    const files = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      if (file.type.startsWith("image/") || file.type.startsWith("video/")) {
+        console.log("Selected file:", file);
+      } else {
+        console.error("Unsupported file type:", file.type);
+        return;
+      }
+    }
+
+    setSelectedFiles([...selectedFiles, ...files]);
+
+    event.target.value = null;
+  }
+
+  const removeFile = (indexToRemove) => {
+    setSelectedFiles(selectedFiles.filter((_, index) => index !== indexToRemove));
+  };
 
   useEffect(() => {
     const handleResize = () => {
@@ -34,7 +63,15 @@ const Home = () => {
   }, []); // Empty dependency array means this effect runs once after the initial render
 
   return (
-    <Layout>  
+    <Layout>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        accept="image/*,video/*"
+        onChange={handleFileChange}
+        multiple
+      />
       {/* <CapsuleInfo />     */}
       {/* <CapsuleList /> */}
       <div id="mainAreaContainer">
@@ -45,18 +82,61 @@ const Home = () => {
                 <input type="text" placeholder="Title"></input>
               </div>
               <div className="formInputBox" id="dateBox">
-              <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} dateFormat="MMMM dd, yyyy" />              </div>
+                <DatePicker
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  dateFormat="MMMM dd, yyyy"
+                />{" "}
+              </div>
               <div className="formInputBox" id="descBox">
                 <textarea placeholder="Description"></textarea>
               </div>
               <div id="uploadContainer">
                 <div>Upload Cover</div>
-                <div>Upload Content</div>
+                <div onClick={handleUploadContentClick}>Upload Content</div>
                 <div>Upload Note</div>
               </div>
               <div className="formInputBox" id="contentBox">
                 <div id="contentWrap">
-                  <p>No content yet...</p>
+                  {selectedFiles.length > 0 && (
+                    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                      <ul>
+                        {selectedFiles.map((file, index) => (
+                          <li key={index}>
+                            {file.type.startsWith("image/") && (
+                              <img
+                                src={URL.createObjectURL(file)}
+                                alt={file.name}
+                                style={{
+                                  maxWidth: 150,
+                                  maxHeight: 150,
+                                  marginRight: 10,
+                                }}
+                              />
+                            )}
+                            {file.type.startsWith("video/") && (
+                              <video
+                                src={URL.createObjectURL(file)}
+                                alt={file.name}
+                                controls
+                                style={{
+                                  maxWidth: 150,
+                                  maxHeight: 150,
+                                  marginRight: 10,
+                                }}
+                              />
+                            )}
+                            <button onClick={() => removeFile(index)}>
+                              Remove
+                            </button>
+                            {file.name}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+
+                  {/* <p>No content yet...</p> */}
                 </div>
               </div>
               <div id="createButton">
@@ -82,7 +162,7 @@ const Home = () => {
             }}
           >
             <div id="middleContainer">
-              <img src={plus} />
+              <img src={plus} alt="plus" />
               <p>New</p>
               <div style={{ display: "flex" }}>
                 {showComponent && <p>Time&nbsp;</p>}
@@ -97,4 +177,3 @@ const Home = () => {
 };
 
 export default Home;
-
